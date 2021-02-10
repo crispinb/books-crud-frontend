5e3728d34bc7dbfa63bf7f35beada6a33eae1237
Crispin Bennett <myself@crisbennett.com>
2021-02-11 09:32:37 +1100

    Strip out cljs-ajax use (doesn't work)

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,9 +1,16 @@
 (ns app.core
-  (:require [reagent.dom :as rdom]))
+  (:require [reagent.dom :as rdom]
+            [ajax.core :refer [GET ajax-request json-response-format]]))
 
-(defn main-component 
+;; TODO: replace cljs-ajax (which I can't get to give me real JSON) with re-frame effects
+;; TODO: get current books displaying with a ajax call, without re-frame or separation
+;; TODO: get backend running from commandline
+;; TODO: add re-frame subs & events (ie. use ajax effects rather than direct ajax)
+;; TODO: separate into components
+
+(defn main-component
   []
-  [:p "we're fone"])
+  [:p "we're done"])
 
 (defn main
   []
@@ -11,5 +18,7 @@
   (rdom/render [main-component]
                (.getElementById js/document "reframe-root")))
 
-(comment 
-  (js/console.log "fark"))
+(comment
+
+  ;;
+  )
