0390a8ea350f88161e5730c21e9a7fa93c4d3011
Crispin Bennett <myself@crisbennett.com>
2021-02-12 14:23:30 +1100

    Refactoring & minor fixes

--- src/main/app/core.cljs
+++ src/main/app/core.cljs
@@ -1,46 +1,15 @@
 (ns app.core
   (:require [reagent.dom :as rdom]
             [re-frame.core :as rf]
-            [day8.re-frame.http-fx]
             [app.components.books :refer [books-component]]
-            [ajax.core :as ajax]))
+            [app.events :refer [register-events]]))
 
-;; TODO: get the book page to refresh!
-;; TODO: tidy up, put events in rght plac, etc.
 ;; TODO: book detail (linked)
 ;; TODO: delete
 ;; TODO: add
 ;; TODO: update
 
-;; TODO: move events to events.cljs
-(rf/reg-event-fx
- :get-books
- (fn [{db :db}]
-   {:http-xhrio {:method :get
-                 :uri "http://localhost:8088/api/books"
-                 :response-format (ajax/json-response-format {:keywords? true})
-                 :on-success [:books-received]
-                 :on-failure [:book-fetch-failed]
-                 }}
-   ))
 
-(rf/reg-event-db
- :books-received
- (fn [db [_ response]]
-   (let [books (js->clj response)]
-     (-> db
-         (assoc :loading? false)
-         (assoc :books books)))))
-
-(rf/reg-event-db
- :book-fetch-failed
- (fn [db [_ response]]
-   (println (str "Book fetch failed with error" response))))
-
-(rf/reg-sub
- :books-changed
- (fn [db]
-   (:books db)))
 
 (defn main-component
   []
@@ -49,14 +18,15 @@
 
 (defn main
   []
-  (js/console.log "dispatching main")
   (rdom/render [main-component]
                (.getElementById js/document "reframe-root")))
 
+;; should these be in main?
+(register-events)
 (rf/dispatch [:get-books])
 
 
 (comment
-  (init))
   ;;
+  )
 
---
+++ src/main/app/events.cljs
@@ -0,0 +1,36 @@
+(ns app.events
+  (:require [re-frame.core :as rf]
+            [ajax.core :as ajax]
+            [day8.re-frame.http-fx]))
+
+;; One file's enough for this simple app
+
+;; how would this typically be done?
+(defn register-events
+  []
+  (rf/reg-event-fx
+   :get-books
+   (fn [{db :db}]
+     {:http-xhrio {:method :get
+                   :uri "http://localhost:8088/api/books"
+                   :response-format (ajax/json-response-format {:keywords? true})
+                   :on-success [:books-received]
+                   :on-failure [:book-fetch-failed]}}))
+
+  (rf/reg-event-db
+   :books-received
+   (fn [db [_ response]]
+     (let [books (js->clj response)]
+       (-> db
+           (assoc :loading? false)
+           (assoc :books books)))))
+
+  (rf/reg-event-db
+   :book-fetch-failed
+   (fn [db [_ response]]
+     (println (str "Book fetch failed with error" response))))
+
+  (rf/reg-sub
+   :books-changed
+   (fn [db]
+     (:books db))))
